42648db820e3e485aba0a44f4ff44ecd81c7d0bd
Cetartiodactyla <31658859+Cetartiodactyla@users.noreply.github.com>
2026-09-09 15:53:57 +0200

    faulancer 0.1.1

--- Formula/faulancer.rb
+++ Formula/faulancer.rb
@@ -3,12 +3,12 @@ require_relative "../lib/private_strategy"
 class Faulancer < Formula
   desc "Notion databases to Premiere Pro timelines (JSX generation)"
   homepage "https://github.com/finally-studio/faulancer"
-  version "0.1.0"
+  version "0.1.1"
   license :cannot_represent
 
-  url "https://github.com/finally-studio/faulancer/releases/download/v0.1.0/faulancer.tar.gz",
+  url "https://github.com/finally-studio/faulancer/releases/download/v0.1.1/faulancer.tar.gz",
       using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "697d3ebd85df6bef952c7557f512a7d26d98c872d97192d2f4565e661a0316fc"
+  sha256 "6b5bca47c1e66ac5f6d6190ad34d33f856b50078fc9a2c2e49e60c22c005c609"
 
   depends_on "python@3.13"
   depends_on :macos
@@ -24,6 +24,8 @@ class Faulancer < Formula
 
     (bin/"faulancer").write <<~SH
       #!/bin/bash
+      export FAULANCER_HOME="${FAULANCER_HOME:-$HOME/Library/Application Support/Faulancer}"
+      cd "#{libexec}" && "#{libexec}/venv/bin/python" -m src.setup
       exec "#{libexec}/venv/bin/python" "#{libexec}/faulancer.py" "$@"
     SH
     chmod 0755, bin/"faulancer"
