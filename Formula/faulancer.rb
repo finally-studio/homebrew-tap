9b4c93ed9e05f4b3fc9eed577bc61783e887d39e
Cetartiodactyla <31658859+Cetartiodactyla@users.noreply.github.com>
2026-09-22 10:49:16 +0200

    faulancer 0.1.4

--- Formula/faulancer.rb
+++ Formula/faulancer.rb
@@ -3,12 +3,12 @@ require_relative "../lib/private_strategy"
 class Faulancer < Formula
   desc "Notion databases to Premiere Pro timelines (JSX generation)"
   homepage "https://github.com/finally-studio/faulancer"
-  version "0.1.3"
+  version "0.1.4"
   license :cannot_represent
 
-  url "https://github.com/finally-studio/faulancer/releases/download/v0.1.3/faulancer.tar.gz",
+  url "https://github.com/finally-studio/faulancer/releases/download/v0.1.4/faulancer.tar.gz",
       using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "ab9ea424e5bbffa89cabbcb669d04bb932d3e63c6736d2b19bc7b9527f0db5ea"
+  sha256 "5ae4117df6af2d33da0e541c5503f78ea3c2e052a9fc8ee0287d90e095e20593"
 
   depends_on "python@3.13"
   depends_on :macos
